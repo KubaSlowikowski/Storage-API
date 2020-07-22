1ccee700e13b66173aca492bd8d3ac03194cfbbd
slowikowskij <jakub.slowikowski@ttsw.com.pl>
2020-07-22 14:35:26 +0200

    Add Swagger2 and Swagger UI.

--- src/main/java/pl/slowikowski/demo/StorageApplication.java
+++ src/main/java/pl/slowikowski/demo/StorageApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.validation.Validator;
 
 @SpringBootApplication
+@EnableSwagger2
 public class StorageApplication {
 
     public static void main(String[] args) {
@@ -18,4 +20,12 @@ public class StorageApplication {
     Validator validator() {
         return new LocalValidatorFactoryBean();
     }
+
+//    @Bean
+//    public Docket get() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .paths(PathSelectors.ant("/products/**"))
+//                .build();
+//    }
 }
--- src/main/java/pl/slowikowski/demo/abstraction/AbstractController.java
+++ src/main/java/pl/slowikowski/demo/abstraction/AbstractController.java
@@ -1,5 +1,7 @@
 package pl.slowikowski.demo.abstraction;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
