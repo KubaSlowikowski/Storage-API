f678ab2eec14efe6f910aeda8d01b0507a3c3914
slowikowskij <jakub.slowikowski@ttsw.com.pl>
2020-08-12 15:16:55 +0200

    Created soap service for products.

--- src/main/java/pl/slowikowski/demo/soap/WebServiceConfig.java
+++ src/main/java/pl/slowikowski/demo/soap/WebServiceConfig.java
@@ -1,5 +1,10 @@
 package pl.slowikowski.demo.soap;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +17,9 @@ import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
@@ -22,7 +30,6 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean(servlet, "/ws/*");
     }
-
 
 //    PRODUCT:
     @Bean(name = "products")
@@ -55,5 +62,36 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema productGroupsSchema() {
         return new SimpleXsdSchema(new ClassPathResource("/schema/productGroup.xsd"));
+    }
+
+
+    //VERSION WITH SERVICES (WITHOUT ENDPOINTS
+    private static final String BINDING_URI = "http://www.w3.org/2003/05/soap/bindings/HTTP/";
+
+    @Autowired
+    private List<SoapService> endpoints;
+
+    @Bean
+    public ServletRegistrationBean<CXFServlet> disServlet() {
+        return new ServletRegistrationBean<>(new CXFServlet(), "/soap-api/*");
+    }
+
+    @Bean(name = Bus.DEFAULT_BUS_ID)
+    public SpringBus springBus() {
+        SpringBus bus = new SpringBus();
+        bus.setProperty("org.apache.cxf.stax.maxTextLength", 1024 * 1024 * 1024);
+        return bus;
+    }
+
+    @PostConstruct
+    public void init() {
+        for (SoapService bean : endpoints) {
+            if (bean.getClass().getAnnotation(SoapEndpoint.class) == null) {
+                throw new IllegalArgumentException("Missed @SoapEndpoint for " + bean.getClass().getName());
+            }
+            EndpointImpl endpoint = new EndpointImpl(springBus(), bean);
+            endpoint.setBindingUri(BINDING_URI);
+            endpoint.publish(bean.getClass().getAnnotation(SoapEndpoint.class).publish());
+        }
     }
 }
