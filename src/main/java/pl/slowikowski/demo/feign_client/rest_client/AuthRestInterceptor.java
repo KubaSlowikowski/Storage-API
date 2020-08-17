9e47f0c836f1934835ca29e8e114302b03d55233
slowikowskij <jakub.slowikowski@ttsw.com.pl>
2020-08-17 14:54:54 +0200

    Extracted admin credentials to application.properties.

--- src/main/java/pl/slowikowski/demo/feign_client/rest_client/AuthRestInterceptor.java
+++ src/main/java/pl/slowikowski/demo/feign_client/rest_client/AuthRestInterceptor.java
@@ -2,23 +2,30 @@ package pl.slowikowski.demo.feign_client.rest_client;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import pl.slowikowski.demo.feign_client.CredentialsLoader;
 import pl.slowikowski.demo.feign_client.authorization.AuthLibraryClient;
 import pl.slowikowski.demo.feign_client.authorization.AuthLibraryRequest;
 
 @Configuration
 @Slf4j
-@RequiredArgsConstructor
 public class AuthRestInterceptor implements RequestInterceptor { //feign clients requests only
+    private String username;
+    private String password;
     private final AuthLibraryClient authClient;
+
+    public AuthRestInterceptor(final AuthLibraryClient authClient) {
+        this.authClient = authClient;
+        this.username = CredentialsLoader.getUsername();
+        this.password = CredentialsLoader.getPassword();
+    }
 
     @Override
     public void apply(RequestTemplate template) {
         var authRequest = new AuthLibraryRequest();
-        authRequest.setUsername("admin");
-        authRequest.setPassword("adminadmin");
+        authRequest.setUsername(username);
+        authRequest.setPassword(password);
         var token = authClient.authenticateUser(authRequest).getToken();
 
         template.header("Authorization", "Bearer " + token);
--- src/main/java/pl/slowikowski/demo/feign_client/soap_client/AuthSoapInterceptor.java
+++ src/main/java/pl/slowikowski/demo/feign_client/soap_client/AuthSoapInterceptor.java
@@ -5,6 +5,7 @@ import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import pl.slowikowski.demo.feign_client.CredentialsLoader;
 import pl.slowikowski.demo.feign_client.authorization.AuthLibraryClient;
 import pl.slowikowski.demo.feign_client.authorization.AuthLibraryRequest;
 
@@ -14,12 +15,17 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class AuthSoapInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    private String username;
+    private String password;
     private final AuthLibraryClient authClient;
 
     //https://cxf.apache.org/docs/interceptors.html
     public AuthSoapInterceptor(final AuthLibraryClient authClient) {
         super(Phase.USER_LOGICAL);
         this.authClient = authClient;
+        this.username = CredentialsLoader.getUsername();
+        this.password = CredentialsLoader.getPassword();
     }
 
     @Override
@@ -29,11 +35,19 @@ public class AuthSoapInterceptor extends AbstractPhaseInterceptor<Message> {
             protocolHeaders = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         }
         var authRequest = new AuthLibraryRequest();
-        authRequest.setUsername("admin");
-        authRequest.setPassword("adminadmin");
+        authRequest.setUsername(username);
+        authRequest.setPassword(password);
         var token = authClient.authenticateUser(authRequest).getToken();
         protocolHeaders.put("Authorization", Collections.singletonList("Bearer " + token));
 
         message.put(Message.PROTOCOL_HEADERS, protocolHeaders);
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
