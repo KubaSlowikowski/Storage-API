009f0539b049807111a4becbdd9bcc48d72f1544
slowikowskij <jakub.slowikowski@ttsw.com.pl>
2020-08-19 18:19:23 +0200

    Extracted methods in PdfBuilder.

--- src/main/java/pl/slowikowski/demo/export/pdf/abstraction/PdfBuilder.java
+++ src/main/java/pl/slowikowski/demo/export/pdf/abstraction/PdfBuilder.java
@@ -19,6 +19,9 @@ public class PdfBuilder<D extends AbstractDto> {
     private String title;
     private List<String> fieldsToIgnore;
     private PdfPTable table;
+    private Document document;
+    private ByteArrayOutputStream out;
+    private List<Field> fields;
 
     private PdfBuilder() {
     }
@@ -44,9 +47,34 @@ public class PdfBuilder<D extends AbstractDto> {
 
     public ByteArrayInputStream build() {
 
-        List<Field> fields = new ArrayList<>(List.of(AbstractDto.class.getDeclaredFields()));
+        fields = new ArrayList<>(List.of(AbstractDto.class.getDeclaredFields()));
         fields.addAll(List.of(dtos.get(0).getClass().getDeclaredFields()));
 
+        ignoreFields();
+
+        table = new PdfPTable(fields.size());
+        document = new Document();
+        out = new ByteArrayOutputStream();
+
+        try {
+            PdfWriter.getInstance(document, out);
+            createDocument();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+
+    private void createDocument() throws DocumentException {
+        document.open();
+        addTitle();
+        initTableHeaders();
+        fillCells();
+        document.add(table);
+        document.close();
+    }
+
+    private void ignoreFields() {
         if (fieldsToIgnore != null) {
             for (String ignore : fieldsToIgnore) {
                 Set<Field> toRemove = fields.stream()
@@ -57,50 +85,40 @@ public class PdfBuilder<D extends AbstractDto> {
                 }
             }
         }
+    }
 
-        //table size
-        table = new PdfPTable(fields.size());
+    private void addTitle() throws DocumentException {
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Paragraph paragraph = new Paragraph(title, font);
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        document.add(paragraph);
+        document.add(Chunk.NEWLINE);
+    }
 
-        Document document = new Document();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try {
-            PdfWriter.getInstance(document, out);
-            document.open();
+    private void initTableHeaders() {
+        fields.forEach(field -> {
+            PdfPCell header = new PdfPCell();
+            Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+            header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            header.setHorizontalAlignment(Element.ALIGN_CENTER);
+            header.setBorderWidth(1);
+            header.setPhrase(new Phrase(field.getName(), headerFont));
+            table.addCell(header);
+        });
+    }
 
-            //add text to pdf file
-            Font font = FontFactory.getFont(FontFactory.COURIER, 17, BaseColor.BLACK);
-            Paragraph paragraph = new Paragraph(title, font);
-            paragraph.setAlignment(Element.ALIGN_CENTER);
-            document.add(paragraph);
-            document.add(Chunk.NEWLINE);
-
-            //make column titles
+    private void fillCells() {
+        Font cellFont = FontFactory.getFont(FontFactory.TIMES_ITALIC, 10, BaseColor.BLACK);
+        for (D dto : dtos) {
             fields.forEach(field -> {
-                PdfPCell header = new PdfPCell();
-                Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                header.setBorderWidth(1);
-                header.setPhrase(new Phrase(field.getName(), headerFont));
-                table.addCell(header);
+                try {
+                    field.setAccessible(true);
+                    createCell(String.valueOf(field.get(dto)), cellFont);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
             });
-            Font cellFont = FontFactory.getFont(FontFactory.TIMES_ITALIC, 10, BaseColor.BLACK);
-            for (D dto : dtos) {
-                fields.forEach(field -> {
-                    try {
-                        field.setAccessible(true);
-                        createCell(String.valueOf(field.get(dto)), cellFont);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-            document.add(table);
-            document.close();
-        } catch (DocumentException e) {
-            e.printStackTrace();
         }
-        return new ByteArrayInputStream(out.toByteArray());
     }
 
     private void createCell(String cellData, Font cellFont) {
